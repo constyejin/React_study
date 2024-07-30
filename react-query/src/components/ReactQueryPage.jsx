17652b3997af648361de2d018b8b014546f36872
constyejin <lightup.jin@gmail.com>
2024-07-30 10:00:00 +0800

    Separate into the fetchpost function

--- react-query/src/components/ReactQueryPage.jsx
+++ react-query/src/components/ReactQueryPage.jsx
@@ -4,25 +4,22 @@ import React from 'react';
 
 const ReactQueryPage = () => {
   // queryFn 함수로 분리 가능
-  // const fetchPost = () => {
-  //   return axios.get('http://localhost:3004/posts');
-  // }
+  const fetchPost = () => {
+    return axios.get('http://localhost:3004/posts');
+  }
   
   const { isLoading, data, isError, error } = useQuery({
     // API 호출에 유니크한 이름을 부여할 수 있다.
     queryKey: ['posts'],
-    queryFn: () => {
-      return axios.get('http://localhost:3004/posts');
-    },
+    queryFn: fetchPost,
     // API 호출 에러시 재시도 기본 횟수 3회 (retry로 원하는만큼 지정 가능)
     retry: 1,
+    // 받은 전체 데이터에서 date만 return 
     select: (data) => {
-      return data.data
-    }
-  });
-
-  console.log('DATA', isLoading,data);
-  console.log('ERROR', isError, error);
+        return data.data
+      }
+    });
+    
 
   if(isLoading) {
     return <h1>Loading...</h1>
