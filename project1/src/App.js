420d4ac353070155da181c3fd8504978a152a282
constyejin <lightup.jin@gmail.com>
2024-08-13 21:12:44 +0900

    ✨ 초기화 및 가위바위보 프로젝트 시작

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
+// 1. 박스 2개 (타이틀, 사진, 결과)
+// 2. 가위 바위 보 버튼
+// 3. 버튼을 클릭하면 클릭한 값이 박스에 보인다.
+// 4. 컴퓨터는 랜덤하게 아이템이 선택된다.
+// 5. 3,4 결과로 누가 이겼는지 승패를 따진다.
+// 6. 승패 결과에 따라 박스 border 색 변경 (win - 초록 | lose - 빨강 | tie - 검정)
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
     </div>
   );
 }
