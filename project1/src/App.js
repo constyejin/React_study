ca722559571aec2af87de1802762137d62e79c0d
constyejin <lightup.jin@gmail.com>
2024-08-13 23:04:37 +0900

    ✨ userSelect, computerSelect 함수 구현

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,5 +1,12 @@
 import {useState} from 'react';
 import './App.css';
+import Box from './component/Box';
+
+// 가위, 바위, 보 이미지 import
+import rock from './rock.png';
+import scissors from './images/scissors.png';
+import paper from './images/paper.png';
+
 
 // 1. 박스 2개 (타이틀, 사진, 결과)
 // 2. 가위 바위 보 버튼
@@ -7,10 +14,50 @@ import './App.css';
 // 4. 컴퓨터는 랜덤하게 아이템이 선택된다.
 // 5. 3,4 결과로 누가 이겼는지 승패를 따진다.
 // 6. 승패 결과에 따라 박스 border 색 변경 (win - 초록 | lose - 빨강 | tie - 검정)
+
+const choice = {
+  rock : {
+    name : "Rock",
+    img : rock,
+  },
+  scissors: {
+    name : "Scissors",
+    img : scissors,
+  },
+  paper : {
+    name : "Paper",
+    img : paper,
+  },
+}
+
 function App() {
+  const [userSelect, setUserSelect] = useState(null);
+  const [comSelect, setComSelect] = useState(null);
+
+  const play = (userChoice) => {
+    setUserSelect(choice[userChoice]);
+    randomChoice();
+  }
+
+  const randomChoice = () => {
+    let itemArr = Object.keys(choice);
+    let randomItem = Math.floor(Math.random() * itemArr.length);
+    let final = itemArr[randomItem]
+    setComSelect(choice[final]);
+  }
+
   return (
     <div>
+      <div className="main">
+        <Box title="You" item={userSelect}/>
+        <Box title="Computer" item={comSelect}/>
+      </div>
 
+      <div className='choice-btn'>
+        <button onClick={() => play("scissors")}>가위</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>
+      </div>
     </div>
   );
 }
