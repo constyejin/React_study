11f1546be3cf9d56f344cac00aad36fe03095fe8
constyejin <lightup.jin@gmail.com>
2024-08-14 20:00:40 +0900

    ✨ Computer result 결과 반전 시키기

--- project1/src/component/Box.jsx
+++ project1/src/component/Box.jsx
@@ -7,7 +7,7 @@ const boxDefault = {
 
   },
   computer : {
-    name : "loigi",
+    name : "luigi",
     img : "https://i.namu.wiki/i/IPBNbStIHLetV6UJnHJhRonRoA8s0SeYaAOz4_sceHUof-y9bxiCLxH8aM33qsPDiLaCO8SZ0ugXD4Kh6JHROw.webp"
   }
 }
@@ -15,13 +15,23 @@ const boxDefault = {
 
 const Box = (props) => {
   const {title} = props;
+  let {result} = props;
+  console.log(title, result)
+
+  if(title === "Computer" && result !== "TIE" && result !== "") {
+    result = result === "WIN" ? "LOSE" : "WIN";
+  } else {
+    result = result;
+  }
+
+  const resultBgc = result.toLowerCase();
   const defalutImg = title === "You" ? boxDefault.user : boxDefault.computer;
   
   return (
-    <div className='box'>
+    <div className={`box ${resultBgc}`}>
       <h1>{props.title}</h1>
       <img src={props.item ? props.item.img : defalutImg.img} alt={props.item ? props.item.name : defalutImg.name} />
-      <h2>{props.result}</h2>
+      <h2>{result}</h2>
     </div>
   )
 }
