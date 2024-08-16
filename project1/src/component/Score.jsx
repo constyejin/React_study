0a0e0ce69183e9e0df05610dcf8dd2cb8f49843c
constyejin <lightup.jin@gmail.com>
2024-08-16 22:33:52 +0900

    💄 Add .pos-center-y class to score-timer And score-reset

--- project1/src/component/Score.jsx
+++ project1/src/component/Score.jsx
@@ -3,11 +3,18 @@ import React from 'react'
 const Score = (props) => {
   return (
     <div className='score-box'>
-      {/* <h3>TIMER</h3> */}
-      <div className='user-score'>{props.score.userScore}</div>
-      <span>:</span>
-      <div className="com-score">{props.score.comScore}</div>
-      <button onClick={props.reset} className='reset-btn'>RESET</button>
+      <div className='score-timer pos-center-y'>
+        <p>TIMER : 10</p>
+
+      </div>
+
+      <div className='score-num'>
+        <div className='user-score'>{props.score.userScore}</div>
+        <span>:</span>
+        <div className="com-score">{props.score.comScore}</div>
+      </div>
+
+      <button onClick={props.reset} className='reset-btn pos-center-y'>RESET</button>
     </div>
   )
 }
