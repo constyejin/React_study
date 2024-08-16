fae14bada12d55badc16aa2f3e7c333b6bcf2f8a
constyejin <lightup.jin@gmail.com>
2024-08-16 14:57:06 +0900

    🔥 Timer 주석처리

--- project1/src/component/Score.jsx
+++ project1/src/component/Score.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Score = (props) => {
   return (
     <div className='score-box'>
-      <h3>TIMER</h3>
+      {/* <h3>TIMER</h3> */}
       <div className='user-score'>{props.score.userScore}</div>
       <span>:</span>
       <div className="com-score">{props.score.comScore}</div>
