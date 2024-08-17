e4b1fe11ee5254955ef51f952cdfb9d2b5820058
constyejin <lightup.jin@gmail.com>
2024-08-18 00:00:01 +0900

    ✨ Creage WeatherBox Component And Change to one useState

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,16 +1,10 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faSun, faCloud, faUmbrella} from "@fortawesome/free-solid-svg-icons";
+import WeatherBox from './component/WeatherBox';
 
 
 function App() {
-  const [name, setName] = useState('');
-  const [cel, setCel] = useState(0);
-  const [fah, setFah] = useState(0);
-  const [weather, setWeather] = useState('');
-  const [humid, setHumid] = useState('');
-  const [wind, setWind] = useState('');
+  const [weather, setWeather] = useState(null);
 
   const getCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition((pos) => {
@@ -22,28 +16,11 @@ function App() {
   }
 
   const getWeatherByCurrentLocation = async(lat, lon, apiKey) => {
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    // units=metric
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
     let response = await fetch(url);
     let data = await response.json();
-
-    let name = data.name.toUpperCase();
-    setName(name);
-
-    // 켈빈(Kelvin), ℃ 섭씨(Celsius), ℉ 화씨(Fahrenheit)
-    let kel = data.main.temp;
-    let cel = (kel - 273.15).toFixed(2);
-    let fah = ((kel - 273.15) * 9 / 5 + 32).toFixed(2);
-    setCel(cel);
-    setFah(fah);
-
-    let weather = data.weather[0].description.toUpperCase();
-    setWeather(weather);
-
-    let humidity = data.main.humidity;
-    setHumid(humidity);
-
-    let windSpeed = data.wind.speed;
-    setWind(windSpeed);
+    setWeather(data)
   }
 
   useEffect(() => {
@@ -52,30 +29,7 @@ function App() {
 
   return (
     <div className='wrapper cloudy'>
-      <div>
-        <div className='box'>
-          <h1>{name}</h1>
-          <div className='weather-icon'>
-            <FontAwesomeIcon icon={faCloud} />
-          </div>
-          <div className='temp'>
-            <p>{cel} ℃</p>
-            <span>|</span>
-            <p>{fah} ℉</p>
-          </div>
-          <div className='weather-txt'>{weather}</div>
-        </div>
-        <div className='box'>
-          <div className='humid'>
-            <p>{humid}%</p>
-            <p>Humidity</p>
-          </div>
-          <div className='wind'>
-            <p>{wind}</p>
-            <p>Wind-Speed</p>
-          </div>
-        </div>
-      </div>
+      <WeatherBox weather={weather} />
     </div>
   );
 }
---
+++ weather/src/component/WeatherBox.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faSun, faCloud, faUmbrella} from "@fortawesome/free-solid-svg-icons";
+
+const WeatherBox = ({weather}) => {
+  console.log(weather)
+  // 켈빈(Kelvin), ℃ 섭씨(Celsius), ℉ 화씨(Fahrenheit)
+  let fah = ((weather?.main.temp * 9 / 5) + 32).toFixed(2);
+  return (
+    <div>
+      <div className='box'>
+        <h1>{weather?.name}</h1>
+        <div className='weather-icon'>
+          <FontAwesomeIcon icon={faCloud} />
+        </div>
+
+        <div className='temp'>
+          <p>{weather?.main.temp} ℃</p>
+          <span>|</span>
+          <p>{fah} ℉</p>
+        </div>
+
+        <div className='weather-txt'>
+          {weather?.weather[0].main}
+        </div>
+      </div>
+
+      <div className='box'>
+        <div className='humid'>
+          <p>{weather?.main.humidity}%</p>
+          <p>Humidity</p>
+        </div>
+        <div className='wind'>
+          <p>{weather?.wind.speed}</p>
+          <p>Wind-Speed</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default WeatherBox
