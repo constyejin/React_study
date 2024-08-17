5b78c4d056a85c29d9729c4ed21766ce6b9237d0
constyejin <lightup.jin@gmail.com>
2024-08-18 01:11:04 +0900

    ✨ Create WeatherBtn Component

---
+++ weather/src/component/WeatherBtn.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Button } from 'react-bootstrap';
+
+const WeatherBtn = () => {
+  return (
+    <div className='weather-btn-box'>
+      <Button variant="outline-dark">Dark</Button>
+    </div>
+  )
+}
+
+export default WeatherBtn
