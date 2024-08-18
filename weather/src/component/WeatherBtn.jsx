767b1d59e44443bcc83e78ca6c8c85f3ac931aff
constyejin <lightup.jin@gmail.com>
2024-08-19 01:01:58 +0900

    ✨ Passing the Current Location button click value

--- weather/src/component/WeatherBtn.jsx
+++ weather/src/component/WeatherBtn.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 
 const WeatherBtn = ({cities, setCity}) => {
   return (
     <div className='weather-btn-box'>
-      <Button variant="light" onClick={() => {}}>Current Location</Button>
+      <Button 
+        variant="light" 
+        onClick={() => setCity('')}>Current Location</Button>
       
-      {
-        cities.map((item, index) => (
-          <Button 
-            variant="light" 
-            key={index}  
-            onClick={() => {setCity(item)}}>
-            {item.charAt(0).toUpperCase() + item.slice(1)}
-          </Button>
-        ))
-      }
+      {cities.map((item, index) => (
+        <Button 
+          variant="light" 
+          key={index}  
+          onClick={() => setCity(item)}>
+          {item.charAt(0).toUpperCase() + item.slice(1)}
+        </Button>
+        ))}
     </div>
-  )
+  ) 
 }
 
 export default WeatherBtn
