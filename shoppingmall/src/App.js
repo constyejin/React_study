fa7da4ba5f6f3434f5d3e5802577e743b4af0f83
constyejin <lightup.jin@gmail.com>
2024-08-21 00:40:20 +0900

    ✨ Create and Import ProductAll, ProductDetail, Login Components

--- shoppingmall/src/App.js
+++ shoppingmall/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import ProductAll from './page/ProductAll';
+import ProductDetail from './page/ProductDetail';
+import Login from './page/Login';
 
 function App() {
   return (
     <div>
-
+      <Routes>
+        <Route path='' element={} />
+        <Route path='' element={} />
+        <Route path='' element={} />
+      </Routes>
     </div>
   );
 }
