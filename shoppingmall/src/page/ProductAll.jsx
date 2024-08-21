61dc613fd3fc2d0797d2a3e21386946bda771664
constyejin <lightup.jin@gmail.com>
2024-08-22 00:08:54 +0900

    👽️ API Call

--- shoppingmall/src/page/ProductAll.jsx
+++ shoppingmall/src/page/ProductAll.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import ProductCard from '../component/ProductCard';
 
 const ProductAll = () => {
+  const [productList, setProductList] = useState([]);
+
+  const getProducts = async () => {
+    let url = 'http://localhost:4000/products';
+    let response = await fetch(url);
+    let data = await response.json();
+    setProductList(data);
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <div>
-      <h1>전채 상품 페이지</h1>
+      <ProductCard productList={productList}/>
     </div>
   )
 }
