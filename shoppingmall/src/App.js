51942cd7c1e442c9400b433b273b956b198b5a0d
constyejin <lightup.jin@gmail.com>
2024-08-21 20:50:49 +0900

    ✨ Create And Import Home componet

--- shoppingmall/src/App.js
+++ shoppingmall/src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 
 import Navbar from './component/Navbar';
+import Home from './component/Home';
 import ProductAll from './page/ProductAll';
 import Login from './page/Login';
 import ProductDetail from './page/ProductDetail';
@@ -11,7 +12,8 @@ function App() {
     <div>
       <Navbar />
       <Routes>
-        <Route path='/' element={<ProductAll/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/shop' element={<ProductAll/>} />
         <Route path='/login' element={<Login/>} />
         <Route path='/product/:id' element={<ProductDetail/>} />
       </Routes>
