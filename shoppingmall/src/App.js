fdf90c644fe366f82790cf9460241ed004882769
constyejin <lightup.jin@gmail.com>
2024-08-24 00:37:51 +0900

    ✨ Import PrivateRoute Component

--- shoppingmall/src/App.js
+++ shoppingmall/src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
@@ -7,16 +8,22 @@ import Home from './component/Home';
 import ProductAll from './page/ProductAll';
 import Login from './page/Login';
 import ProductDetail from './page/ProductDetail';
+import PrivateRoute from './route/PrivateRoute';
 
 function App() {
+  const [authenticate, setAuthenticate] = useState(false); // true면 로그인
+
+  useEffect(() => {
+    console.log('authenticate', authenticate)
+  }, [authenticate])
   return (
     <div>
       <Gnb />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/shop' element={<ProductAll/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/product/:id' element={<ProductDetail/>} />
+        <Route path='/login' element={<Login setAuthenticate={setAuthenticate}/>} />
+        <Route path='/product/:id' element={<PrivateRoute authenticate={authenticate}/>} />
       </Routes>
     </div>
   );
