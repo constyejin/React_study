57fea869e0303c17d7f40fa8cc9db4972f328515
constyejin <lightup.jin@gmail.com>
2024-08-24 00:06:46 +0900

    📝 Login Component Markup

--- shoppingmall/src/page/Login.jsx
+++ shoppingmall/src/page/Login.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div>
-      <h1>로그인 페이지</h1>
+    <div className='login'>
+      {/* <div className='login-naver'>
+        <img src="https://vendor-cdn.imweb.me/images/naver_login2x.png" alt="naver icon" />
+        <p>네이버로 시작하기</p>
+      </div>
+
+      <div>
+        <img src="https://vendor-cdn.imweb.me/images/kakao_icon.png" alt="kakao icon" />
+        <p>카카오로 시작하기</p>
+      </div> */}
+
+      {/* <div className='login-or'>
+        <div className='line'></div>
+        <div>또는</div>
+        <div className='line'></div>
+      </div> */}
+
+      <form action="" className='login-box'>
+        <div className='login-input-box login-email'>
+          <input type="text" placeholder='이메일'/>
+        </div>
+
+        <div className='login-input-box login-pw'>
+          <input type="password" placeholder='비밀번호'/>
+        </div>
+
+        <div className='login-stay'>
+          <label>
+            <input type="checkbox" />
+            <span>로그인상태유지</span>
+          </label>
+        </div>
+
+        <button type='button' className='login-btn'>로그인</button>
+      </form>
+      
+      <div className='register-find'>
+        <Link to='/register'>회원가입</Link>
+        <div>아이디 · 비밀번호 찾기</div>
+      </div>
     </div>
   )
 }
