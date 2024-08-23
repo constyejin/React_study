4b71c18a0fdec416d0337ab34168750940a28344
constyejin <lightup.jin@gmail.com>
2024-08-24 00:44:48 +0900

    ✨ Set product detail page URL with id value

--- shoppingmall/src/component/ProductCard.jsx
+++ shoppingmall/src/component/ProductCard.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const ProductCard = ({ item }) => {
+  const navigate = useNavigate();
+  const showDetail = () => {
+    navigate(`/product/:{item.id}`);
+  }
   const price = item?.price.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");;
 
   return (
-    <div className='product-card'>
+    <div className='product-card' onClick={showDetail}>
       <div className='product-card-img'>
         <img className='img' src={item?.img.imgCard} alt={item?.title} />
         <img className='img-hover' src={item?.img.imgHover} alt={item?.title} />
