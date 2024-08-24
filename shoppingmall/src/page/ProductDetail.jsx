5469cd8fe4a90b5db1815ace33b3b3ddee56f3dd
constyejin <lightup.jin@gmail.com>
2024-08-25 02:17:14 +0900

    ✨ Create getProductDetail function  to receive detail page URL

--- shoppingmall/src/page/ProductDetail.jsx
+++ shoppingmall/src/page/ProductDetail.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
 
 const ProductDetail = () => {
+  let {id} = useParams();
+
+  const getProductDetail = async () => {
+    let url = `http://localhost:4000/products/${id}`;
+    // let url = `https://my-json-server.typicode.com/constyejin/React_study/products`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+  }
+
+  useEffect(() => {
+    getProductDetail();
+  },[])
   return (
     <div>
       <h1>상품 디테일 페이지</h1>
