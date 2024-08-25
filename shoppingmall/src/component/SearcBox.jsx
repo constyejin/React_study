12600b51c8f53d9338debf6f5d9c4e4c5f3354c5
constyejin <lightup.jin@gmail.com>
2024-08-25 21:49:47 +0900

    ✨ Get value from search input

--- shoppingmall/src/component/SearcBox.jsx
+++ shoppingmall/src/component/SearcBox.jsx
@@ -3,11 +3,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const SearcBox = () => {
+  const search = (e) => {
+    if(e.key === 'Enter') {
+      let keyword = e.target.value;
+    }
+  }
+
   return (
     <div className='search'>
       <div className='search-box'>
         <FontAwesomeIcon icon={faSearch}/>
-        <input type="text"  placeholder='Search'/>
+        <input onKeyPress={search} type="text" placeholder='Search'/>
       </div>
     </div>
 
