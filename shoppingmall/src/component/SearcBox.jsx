6c7eb4db58b88564117983d3be914a75bd3d4945
constyejin <lightup.jin@gmail.com>
2024-08-25 22:06:44 +0900

    ✨ Passing query values using useNavigate

--- shoppingmall/src/component/SearcBox.jsx
+++ shoppingmall/src/component/SearcBox.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
 const SearcBox = () => {
+  const navigate = useNavigate();
   const search = (e) => {
     if(e.key === 'Enter') {
       let keyword = e.target.value;
+      navigate(`/shop/?q=${keyword}`);
     }
   }
 
