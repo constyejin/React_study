b3c4b1388e96bed31db2ce11225d26b4fcf865d7
constyejin <lightup.jin@gmail.com>
2024-08-26 00:06:11 +0900

    ✨ Get state value with useSelect

--- redux-prac/src/App.js
+++ redux-prac/src/App.js
@@ -1,17 +1,17 @@
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const count = useSelector(state => state.count);
   const dispatch = useDispatch();
 
   const increase = () => {
     // 반드시 type이라는 키가 있어야 한다. (payload는 선택사항)
     // type => 액셕의 이름
     dispatch({type: 'INCREMENT'});
-    setCount(count + 1);
   }
+
   return (
     <div>
       <h1>{count}</h1>
