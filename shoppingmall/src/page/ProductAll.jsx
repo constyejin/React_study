d44dbeb1ac4358650e8c1f0e1d1d3e3e483743db
constyejin <lightup.jin@gmail.com>
2024-08-25 21:24:57 +0900

    📝 Add COLLECTION Content

--- shoppingmall/src/page/ProductAll.jsx
+++ shoppingmall/src/page/ProductAll.jsx
@@ -20,6 +20,17 @@ const ProductAll = () => {
     <div className="container">
       <div className="row"></div>
         <div className="col-lg-12">
+          <div className='collection-txt'>
+            <p className='collection-txt-title'>Spring -Summer '24 COLLECTION</p>
+            <p>
+              As an artist, I instinctively began my journey. <br />
+              I never considered following the same path as everyone  <br />
+              else right from the start. Even though I might not be  <br />
+              racing ahead of others, I persistently pursue a  <br />
+              direction that remains true to my artistic vision. 
+            </p>
+          </div>
+
           <div className="product-card-list">
             {
               productList.map((item, index) => {
