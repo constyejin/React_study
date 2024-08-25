0218a8eee80f41b41ebd1215c5fdfddb7fb9345d
constyejin <lightup.jin@gmail.com>
2024-08-26 00:05:35 +0900

    ✨ Increases by 1 when button is clicked

--- redux-prac/src/redux/reducer/reducer.js
+++ redux-prac/src/redux/reducer/reducer.js
@@ -3,7 +3,18 @@ let initialState = {
 };
 
 function reducer(state = initialState, action) {
-  console.log(action);
+  if(action.type === 'INCREMENT') {
+    return {...state, count: state.count + 1}
+  }
+
+  return {...state}
+
+  // switch(action.type) {
+  //   case 'INCREMENT' :
+  //     return {...state, count: state.count + 1};
+  //   default: 
+  //     return {...state}
+  // }
 }
 
 export default reducer;
