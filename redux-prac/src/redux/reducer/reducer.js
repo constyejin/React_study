86d3c22b246df96f4590565bdb2c4ccf6cfee8f0
constyejin <lightup.jin@gmail.com>
2024-08-26 01:12:36 +0900

    ✨ minus 버튼 클릭시 count값 - 1

--- redux-prac/src/redux/reducer/reducer.js
+++ redux-prac/src/redux/reducer/reducer.js
@@ -16,6 +16,8 @@ function reducer(state = initialState, action) {
   switch(action.type) {
     case 'INCREMENT' :
       return {...state, count: state.count + 1};
+    case 'DECREMENT' :
+      return {...state, count: state.count - 1};
     case 'LOGIN' :
       return {
         ...state, 
