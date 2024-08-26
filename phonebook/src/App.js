39f578af39d21978fdc05905677c058effee432a
constyejin <lightup.jin@gmail.com>
2024-08-26 16:23:19 +0900

    ✨ Import Bootstrap

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons'
+
+import ContactList from './components/ContactList';
+import SearchBox from './components/SearchBox';
 
 function App() {
   return (
-    <div>
-
+    <div className='wrapper'>
+      <header>
+        <h1 className='title'>Contacts</h1>
+        {/* <ContactList /> */}
+        <div className='main-right'>
+          <SearchBox />
+          <button className='contact-add-btn'>
+            <FontAwesomeIcon icon={faUserPlus} />
+            New Contacts
+          </button>
+        </div>
+      </header>
     </div>
   );
 }
