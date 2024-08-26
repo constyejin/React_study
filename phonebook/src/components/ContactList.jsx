29c2d351f4fcec1e1f70eec62b704076944ece80
constyejin <lightup.jin@gmail.com>
2024-08-26 22:22:09 +0900

    ✨ props로 <ContactItem>에 item값 전달

--- phonebook/src/components/ContactList.jsx
+++ phonebook/src/components/ContactList.jsx
@@ -3,12 +3,12 @@ import { useSelector } from 'react-redux';
 import ContactItem from './ContactItem';
 
 const ContactList = () => {
-  const contactList = useSelector(state => state.contactList)
-  console.log(contactList);
+  const contactList = useSelector(state => state.contactList);
+
   return (
     <div className='contact-user-list'>
       {
-        contactList.map((item) => <ContactItem /> )
+        contactList.map((item) => <ContactItem item={item} /> )
       }
     </div>
   )
