13628e2ccad7c301936617762285b5bfd28e48fc
constyejin <lightup.jin@gmail.com>
2024-08-26 21:11:08 +0900

    📝  Add .sm-hidden class to .contact-add-btn text

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,27 +1,33 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col } from 'react-bootstrap';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserPlus } from '@fortawesome/free-solid-svg-icons'
 
 import ContactList from './components/ContactList';
 import SearchBox from './components/SearchBox';
+import ContactForm from './components/ContactForm';
+import { useState } from 'react';
 
 function App() {
+  const [modal, setModal] = useState(false);
+
   return (
     <div className='wrapper'>
       <header>
         <h1 className='title'>Contacts</h1>
-        {/* <ContactList /> */}
+        
         <div className='main-right'>
           <SearchBox />
-          <button className='contact-add-btn'>
+          <button onClick={() => setModal(true)} className='contact-add-btn'>
             <FontAwesomeIcon icon={faUserPlus} />
-            New Contacts
+            <span className='sm-hidden'>New Contacts</span>
           </button>
         </div>
       </header>
+      <ContactList />
+      {
+        modal === true ? <ContactForm /> : null
+      }
     </div>
   );
 }
