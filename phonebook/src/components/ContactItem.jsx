a027d545971e0f75641d4149374cb6b0a8542d5c
constyejin <lightup.jin@gmail.com>
2024-08-26 23:29:20 +0900

    ✨ profileImgAlt data binding

--- phonebook/src/components/ContactItem.jsx
+++ phonebook/src/components/ContactItem.jsx
@@ -6,11 +6,10 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 
 const ContactItem = ({item}) => {
-  console.log(item)
   return (
     <div className='contact-user-item'>
       <div className='user-profile'>
-        <img src="https://www.appsflyer.com/wp-content/uploads/2021/11/how-to-increase-daily-active-users.jpg" alt="" />
+        <img src={item.profileImg} alt={item.profileImgAlt} />
       </div>
 
       <h3 className='user-name'>{item.name}</h3>
