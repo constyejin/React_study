957ceaa2fb25d97cee95e517b4e80c51af7c92cb
constyejin <lightup.jin@gmail.com>
2024-08-26 23:49:31 +0900

    ♻️ Add key value to <ContainerItem>

--- phonebook/src/components/ContactList.jsx
+++ phonebook/src/components/ContactList.jsx
@@ -4,11 +4,12 @@ import ContactItem from './ContactItem';
 
 const ContactList = () => {
   const contactList = useSelector(state => state.contactList);
+  console.log(contactList)
 
   return (
     <div className='contact-user-list'>
       {
-        contactList.map((item) => <ContactItem item={item} /> )
+        contactList.map((item, index) => <ContactItem item={item} key={index} /> )
       }
     </div>
   )
