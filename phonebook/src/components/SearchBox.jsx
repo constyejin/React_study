0655095ce3062d30c8ed8b0685ffd179012acdea
constyejin <lightup.jin@gmail.com>
2024-08-27 00:26:44 +0900

    ♻️ onChange 될 때 마다 setKeyword에 value값 전달

--- phonebook/src/components/SearchBox.jsx
+++ phonebook/src/components/SearchBox.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 const SearchBox = () => {
+  let [keyword, setKeyword] = useState('');
+  let dispatch = useDispatch();
+  // let { contact } = useSelector((state) => state);
+
+  const searchbyName = (e) => {
+    if(e.key === 'Enter') {
+      dispatch({ type: 'SEARCH_BY_USERNAME', payload: {keyword} })
+    }
+  }
   return (
     <div className='search-box'>
-      <input type="text" placeholder='Search here' />
+      <input onKeyPress={searchbyName} onChange={e => setKeyword(e.target.value)} type="text" placeholder='이름으로 검색하세요' />
       <FontAwesomeIcon className='search-icon' icon={faSearch} />
     </div>
   )
