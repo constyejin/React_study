332197c78bed1e59ddb2bc815dedbe635fc76484
constyejin <lightup.jin@gmail.com>
2024-08-26 22:22:25 +0900

    ✨ 전달받은 item값 바인딩 (name, phone, email)

--- phonebook/src/components/ContactItem.jsx
+++ phonebook/src/components/ContactItem.jsx
@@ -1,32 +1,33 @@
-import React from 'react'
+import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 
-const ContactItem = () => {
+const ContactItem = ({item}) => {
+  console.log(item)
   return (
     <div className='contact-user-item'>
       <div className='user-profile'>
         <img src="https://www.appsflyer.com/wp-content/uploads/2021/11/how-to-increase-daily-active-users.jpg" alt="" />
       </div>
 
-      <h3 className='user-name'>yejin LEE</h3>
+      <h3 className='user-name'>{item.name}</h3>
 
       <div className='user-info'>
         <div className='user-phone'>
           <div className='user-icon'>
             <FontAwesomeIcon icon={faPhone} />
           </div>
-          <p>+12 1234-2442</p>
+          <p>{item.phone}</p>
         </div>
 
         <div className='user-email'>
           <div className='user-icon'>
             <FontAwesomeIcon icon={faEnvelope} />
           </div>
-          <p>lalalal@gmail.com</p>
+          <p>{item.email}</p>
         </div>
       </div>
     </div>
