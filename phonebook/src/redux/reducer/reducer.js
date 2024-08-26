ed4a4a19d00d149f6388ec4e006416610fa052b7
constyejin <lightup.jin@gmail.com>
2024-08-26 22:18:41 +0900

    ✨ reducer (name, phone, email)

--- phonebook/src/redux/reducer/reducer.js
+++ phonebook/src/redux/reducer/reducer.js
@@ -1,9 +1,28 @@
-let initialState = {
+import { act } from 'react';
 
+let initialState = {
+  contactList: []
 }
 
-function reducer(start = initialState, action) {
+function reducer(state = initialState, action) {
+  const {type, payload} = action;
 
+  switch(type) {
+    case 'ADD_CONTACT' :
+      return {
+        ...state,
+        contactList: [
+          ...state.contactList, 
+          {
+            name: payload.name, 
+            phone: payload.phone, 
+            email: payload.email,
+          },
+        ],
+      };
+      default: 
+        return {...state}
+  }
 }
 
 export default reducer;
