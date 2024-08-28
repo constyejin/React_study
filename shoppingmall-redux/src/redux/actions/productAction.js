7350fb184c9fa762ffcb5e571fe2cb03a6ce3cd9
constyejin <lightup.jin@gmail.com>
2024-08-28 23:53:19 +0900

    ✨ Redux Toolkit

--- shoppingmall-redux/src/redux/actions/productAction.js
+++ shoppingmall-redux/src/redux/actions/productAction.js
@@ -1,9 +1,12 @@
+import { productActions } from '../reducer/productReducer';
+
 function getProducts(searchQuery) {
   return async (dispatch, getState) => {
     let url = `https://my-json-server.typicode.com/constyejin/React_study/products/?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
-    dispatch({type: 'GET_PRODUCT_SUCCESS', payload: {data} })
+    // dispatch({type: 'GET_PRODUCT_SUCCESS', payload: {data} });
+    dispatch(productActions.getAllProducts( {data} ))
   };
 }
 
--- shoppingmall-redux/src/redux/store.js
+++ shoppingmall-redux/src/redux/store.js
@@ -1,11 +1,21 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from '@redux-devtools/extension';
-import { thunk } from 'redux-thunk';
-import rootReducer from './reducer';
+// Redux 버전 업데이트 되면서 createStore 더 이상 지원하지 않음.
+// import { createStore ,applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 
-let store = createStore(
-  rootReducer, 
-  composeWithDevTools(applyMiddleware(thunk))
-);
+import productReducer from './reducer/productReducer';
+import authenticateReducer from './reducer/authenticateReducer';
+
+// let store = createStore(
+//   rootReducer, 
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
+
+// combineReducer, thunk, applyMiddleware, composeWithDevTools => configureStore
+const store = configureStore({
+  reducer: {
+    auth: authenticateReducer,
+    product: productReducer,
+  }
+})
 
 export default store;
