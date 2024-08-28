4b0368afe05ba517e3695183281dc74a426ab559
constyejin <lightup.jin@gmail.com>
2024-08-28 21:56:40 +0900

    ✨ Login authenticateReducer

---
+++ shoppingmall-redux/src/redux/reducer/authenticateReducer.js
@@ -0,0 +1,23 @@
+let initialState = {
+  id: '',
+  pw: '',
+  authenticate: false,
+}
+
+
+function authenticateReducer(state = {initialState}, action) {
+  let {type, payload} = action;
+  switch(type) {
+    case "LOGIN_SUCCESS":
+      return {
+        ...state,
+        id: payload.id,
+        pw: payload.pw,
+        authenticate: true
+      }
+    default:
+      return {...state};
+  }
+}
+
+export default authenticateReducer;
