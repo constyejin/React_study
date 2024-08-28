fc9b1064a059dca5b8a4af455c58028dd31dfed8
constyejin <lightup.jin@gmail.com>
2024-08-28 18:23:37 +0900

    ✨ combineRedux

---
+++ shoppingmall-redux/src/redux/reducer/index.js
@@ -0,0 +1,9 @@
+// combineReducer
+import { combineReducers } from 'redux';
+import authenticateReducer from './authenticateReducer';
+import productReducer from './productReducer';
+
+export default combineReducers({
+  auth: authenticateReducer,
+  product: productReducer,
+});
