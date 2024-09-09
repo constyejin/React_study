2d7dfd36f9bb274b8bfbfa597aabe45c98a6b9d0
constyejin <lightup.jin@gmail.com>
2024-09-09 10:00:00 +0900

    Create Homepage Component

--- react-query/src/App.js
+++ react-query/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { Route, Routes, Link } from 'react-router-dom';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
     <div>
-
+      <nav>
+        <Link to='/'>Homepage</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+      </Routes>
     </div>
   );
 }
