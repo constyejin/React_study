41b410e136fd2be9926fa08095ce65e068e8cfe5
constyejin <lightup.jin@gmail.com>
2024-09-11 10:00:00 +0900

    ReactQueryPage Route

--- react-query/src/App.js
+++ react-query/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import { Route, Routes, Link } from 'react-router-dom';
 import Homepage from './components/Homepage';
+import ReactQueryPage from './components/ReactQueryPage'
 
 function App() {
   return (
     <div>
       <nav>
         <Link to='/'>Homepage</Link>
+        <Link to='/react-query'>ReactQuery</Link>
       </nav>
       <Routes>
         <Route path='/' element={<Homepage />} />
+        <Route path='/react-query' element={<ReactQueryPage />} />
       </Routes>
     </div>
   );
--- react-query/src/components/ReactQueryPage.jsx
+++ react-query/src/components/ReactQueryPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ReactQueryPage = () => {
   return (
     <div>
-      <h1>REACT QUERY PAGE</h1>
+      <h1>REACT-QUERY PAGE</h1>
     </div>
   )
 }
