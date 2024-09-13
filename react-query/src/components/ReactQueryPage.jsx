396ce90267963302569aa64e8ecba0a4c092d284
constyejin <lightup.jin@gmail.com>
2024-09-13 10:00:00 +0900

    Receiving API data with axios

--- react-query/src/components/ReactQueryPage.jsx
+++ react-query/src/components/ReactQueryPage.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import React from 'react';
 
 const ReactQueryPage = () => {
+  // queryFn 함수로 분리 가능
+  // const fetchPost = () => {
+  //   return axios.get('http://localhost:3004/posts');
+  // }
+  
+  const {data} = useQuery({
+    // API 호출에 유니크한 이름을 부여할 수 있다.
+    queryKey: ['posts'],
+    queryFn: () => {
+      return axios.get('http://localhost:3004/posts');
+    }
+  })
+  console.log(data)
   return (
     <div>
       <h1>REACT-QUERY PAGE</h1>
