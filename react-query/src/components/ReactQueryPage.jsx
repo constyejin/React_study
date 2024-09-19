973fcb04f5fed8a9982b85de8149d8b780c6e5ca
constyejin <lightup.jin@gmail.com>
2024-09-19 10:00:00 +0900

    data bingding

--- react-query/src/components/ReactQueryPage.jsx
+++ react-query/src/components/ReactQueryPage.jsx
@@ -12,27 +12,24 @@ const ReactQueryPage = () => {
     // API 호출에 유니크한 이름을 부여할 수 있다.
     queryKey: ['posts'],
     queryFn: () => {
-      return axios.get('http://localhost:3004/post');
+      return axios.get('http://localhost:3004/posts');
     },
     // API 호출 에러시 재시도 기본 횟수 3회 (retry로 원하는만큼 지정 가능)
     retry: 1,
-  })
+  });
+
+  console.log('DATA', isLoading,data);
+  console.log('ERROR', isError, error);
 
   if(isLoading) {
     return <h1>Loading...</h1>
   }
-
   if(isError) {
     return <h1>{error.message}</h1>
   }
-
-  console.log(isLoading,data);
-  console.log('ERROR', isError, error);
-  return (
-    <div>
-      <h1>REACT-QUERY PAGE</h1>
-    </div>
-  )
-}
+  return <div>{data.data.map(item => (
+    <div>{item.title}</div>
+  ))}</div>;
+};
 
 export default ReactQueryPage
