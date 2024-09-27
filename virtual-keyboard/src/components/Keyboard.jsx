38897b3cada2541d635508492e435579440d3f55
constyejin <lightup.jin@gmail.com>
2024-09-27 10:00:00 +0800

    Fix ths delete-left icon

--- virtual-keyboard/src/components/Keyboard.jsx
+++ virtual-keyboard/src/components/Keyboard.jsx
@@ -1,6 +1,10 @@
 // keyboard.js
 import React, { useState } from 'react';
 
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faDeleteLeft } from '@fortawesome/free-solid-svg-icons'
+
+
 export default function Keyboard() {
     const [inputText, setInputText] = useState('');
     const [isCaps, setIsCaps] = useState(false);
