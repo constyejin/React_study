af8b39b0c813e8852bed7ee1d10fe86b7cba561c
constyejin <lightup.jin@gmail.com>
2024-10-09 19:08:43 +0800

    ✨ Create Footer.jsx Component

---
+++ travel-blog/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='journal-footer'>
+      <caption>Ingrid G. 2023 |</caption>
+      <caption className='footer-caption'> Favicon by: </caption>
+    </div>
+  )
+}
+
+export default Footer;
