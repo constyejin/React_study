b53bdbd7aee760d4394c6ba1528301998217dadf
constyejin <lightup.jin@gmail.com>
2024-10-09 19:08:09 +0800

    ♻️ data.map()

--- travel-blog/src/App.js
+++ travel-blog/src/App.js
@@ -1,10 +1,20 @@
 import Header from './components/Header';
+import Cards from './components/Card';
+import Footer from './components/Footer';
+import { data } from './data';
+
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      {
+        data.map((item) => {
+          return <Cards key={item.id} item={item} />
+        })
+      }
+      <Footer />
     </div>
   );
 }
